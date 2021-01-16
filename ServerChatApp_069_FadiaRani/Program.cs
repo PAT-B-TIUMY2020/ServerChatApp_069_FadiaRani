178032e68fdc5a7f79ce2023bac1d2d71bbd634a
Fadia Rani <fadiarani9@gmail.com>
2021-01-16 19:04:50 +0700

    Add project files.

---
+++ ServerChatApp_069_FadiaRani/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WCFService_2Way_069;
+
+
+
+namespace ServerChatApp_069_FadiaRani
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                ServiceHost hostObjek = null;
+                try
+                {
+                    hostObjek = new ServiceHost(typeof(ServiceCallback));
+                    hostObjek.Open();
+
+
+                    Console.WriteLine("Server On, Ready to Use");
+                    Console.ReadLine();
+
+                    hostObjek.Close();
+                }
+                catch (Exception ex)
+                {
+                    hostObjek = null;
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
